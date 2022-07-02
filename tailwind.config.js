51797d5885919723d1505ec26001147cdceafe72
nursyahrina <nursyahrina17@gmail.com>
2022-07-03 00:33:04 +0700

    tailwind and conf

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}','./src/**/*.{html,js}'],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: '1.5rem',
+    }
+  },
+  plugins: [],
+}
