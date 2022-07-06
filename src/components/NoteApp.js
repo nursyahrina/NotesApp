c2b1807e9d86c29327b53a24b8b0dd29ac7c73f1
nursyahrina <nursyahrina17@gmail.com>
2022-07-07 02:11:13 +0700

    fix search function: move input to app level

--- src/components/NoteApp.js
+++ src/components/NoteApp.js
@@ -2,19 +2,19 @@ import React from "react";
 import { getInitialData } from "../utils";
 import NoteInput from "./NoteInput";
 import NoteList from "./NoteList";
-import NoteSearch from "./NoteSearch";
 
 class NoteApp extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       notes: getInitialData(),
+      keyword: ''
     }
   
     this.onArchiveHandler = this.onArchiveHandler.bind(this);
     this.onDeleteHandler = this.onDeleteHandler.bind(this);
     this.onAddNoteHandler = this.onAddNoteHandler.bind(this);
-    this.onSearchNoteHandler = this.onSearchNoteHandler(this);
   }
   
   onArchiveHandler(id) {
@@ -25,16 +25,17 @@ class NoteApp extends React.Component {
     } else {
       notes[noteIdx].archived = true;
     }
-    this.setState({ notes });
+    this.setState({ notes, keyword: this.state.keyword });
   }
 
   onDeleteHandler(id) {
     const notes = this.state.notes.filter(note => note.id !== id);
-    this.setState({ notes });
+    this.setState({ notes, keyword: this.state.keyword });
   }
 
   onAddNoteHandler({ title, body }) {
     this.setState((prevState) => ({
+      ...prevState,
       notes: [
         ...prevState.notes,
         {
@@ -48,20 +49,22 @@ class NoteApp extends React.Component {
     }));
   }
 
-  onSearchNoteHandler({ keyword }) {
-    const notes = this.state.notes.filter(note => note.title.includes(keyword));
-    this.setState({ notes });
+  onKeywordChangeEventHandler(event) {
+    this.setState((prevState) => ({
+      ...prevState,
+      keyword: event.target.value,
+    }));
   }
   
   render() {
     return (
       <div className="contact-app bg-slate-50">
-        <div className="contact-app__header sticky top-0 z-10 px-5 py-5 bg-indigo-700 drop-shadow-xl">
+        <div className="contact-app__header sticky top-0 z-10 flex flex-wrap gap-4 justify-between px-5 py-5 bg-indigo-700 drop-shadow-xl">
           <div className="flex justify-start">
             <img className="flex-none w-12 h-12 rounded-lg" src="/images/logo.png" alt="NoteApp Logo" />
             <h1 className="flex-initial px-3 text-4xl text text-sky-100">Rubie's NotesApp</h1>
-            <NoteSearch searchNote={this.onSearchNoteHandler}/>
           </div>
+          <input className="mb-2 py-2 px-6 border border-indigo-800 rounded-lg drop-shadow-sm" type="text" placeholder="Search Note Here..." value={this.state.keyword} onChange={this.onKeywordChangeEventHandler.bind(this)} />
         </div>
         <div className="contact-app__container">
           <div className="contact-app__form container max-w-2xl my-6 py-8">
@@ -70,11 +73,11 @@ class NoteApp extends React.Component {
           </div>
           <div className="contact-app__active-notes container max-w-6xl mb-8 py-8 border-t-2 border-indigo-700">
             <h2 className="text-3xl font-bold ml-6 mb-4">Active Notes</h2>
-            <NoteList notes={this.state.notes.filter(note => !note.archived)} onArchive={this.onArchiveHandler} onDelete={this.onDeleteHandler} />
+            <NoteList notes={this.state.notes.filter(note => !note.archived)} onArchive={this.onArchiveHandler} onDelete={this.onDeleteHandler} query={this.state.keyword} />
           </div>
           <div className="contact-app__archived-notes container max-w-6xl mb-8 py-8 border-y-2 border-indigo-700">
             <h2 className="text-3xl font-bold ml-6 mb-4">Archived Notes</h2>
-            <NoteList notes={this.state.notes.filter(note => note.archived)} onArchive={this.onArchiveHandler} onDelete={this.onDeleteHandler} />
+            <NoteList notes={this.state.notes.filter(note => note.archived)} onArchive={this.onArchiveHandler} onDelete={this.onDeleteHandler} query={this.state.keyword} />
           </div>
         </div>
         <div className="contact-app__footer px-5 py-5 bg-slate-900 text-indigo-200 drop-shadow-xl text-center">
--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,11 +1,11 @@
 import React from "react";
 import NoteItem from "./NoteItem";
 
-function NoteList({ notes, onArchive, onDelete }) {
+function NoteList({ notes, onArchive, onDelete, query }) {
   return (
     <div className="note-list grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
       {
-        notes.map((note) => (
+        notes.filter((note) => note.title.toLowerCase().includes(query.toLowerCase())).map((note) => (
           <NoteItem 
           key={note.id}
           id={note.id}
