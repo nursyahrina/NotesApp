529e6e1801f4b36c5466025ae8ea8de974f9df26
nursyahrina <nursyahrina17@gmail.com>
2022-07-07 02:24:12 +0700

    add conditional rendering for note list

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -2,20 +2,29 @@ import React from "react";
 import NoteItem from "./NoteItem";
 
 function NoteList({ notes, onArchive, onDelete, query }) {
-  return (
-    <div className="note-list grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
-      {
-        notes.filter((note) => note.title.toLowerCase().includes(query.toLowerCase())).map((note) => (
-          <NoteItem 
-          key={note.id}
-          id={note.id}
-          onArchive={onArchive}
-          onDelete={onDelete}
-          {...note} />
-        ))
-      }
-    </div>
-  );
+  const filtered = notes.filter((note) => note.title.toLowerCase().includes(query.toLowerCase()));
+  if (filtered.length > 0) {
+    return (
+      <div className="note-list grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+        {
+          filtered.map((note) => (
+            <NoteItem 
+            key={note.id}
+            id={note.id}
+            onArchive={onArchive}
+            onDelete={onDelete}
+            {...note} />
+          ))
+        }
+      </div>
+    );
+  } else {
+    return (
+      <p className="text-slate-500 ml-6 text-lg">
+        No available notes.
+      </p>
+    );
+  } 
 }
 
 export default NoteList;
