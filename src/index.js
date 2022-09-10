fb127da8fdef74bf1aab93ddd0f163dba277fb28
nursyahrina <nursyahrina17@gmail.com>
2022-09-11 03:50:31 +0700

    modify app for SPA

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import NoteApp from './components/NoteApp';
 
 import './styles/index.css';
 
 const root = createRoot(document.getElementById('root'));
-root.render(<NoteApp />);
+root.render(
+  <BrowserRouter>
+    <NoteApp />
+  </BrowserRouter>,
+);
